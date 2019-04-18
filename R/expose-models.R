0c1c84cd1418d92e1117217a3b517829460bb032
Andrew Manderson <a.manderson@live.co.uk>
2019-04-18 14:23:42 +0100

    don't need these anymore

--- R/expose-models.R
+++
@@ -1,8 +0,0 @@
-#' Expose the standard normal model
-#' 
-#' This is just for testing
-#'
-#' @export
-normal_stanmodel <- function() {
-  .stan_models$normal
-}
--- tests/testthat/test_expose-models.R
+++
@@ -1,6 +0,0 @@
-context("Check to see if model compilation worked")
-
-expect_s4_class(
-  object = normal_stanmodel(),
-  class = "stanmodel"
-)
